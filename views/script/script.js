890b0922b9459116e839fb65d0648c7dd78d9a44
sam nurollahi <samnurollahi>
2025-11-06 15:02:56 +0330

    end

--- views/script/script.js
+++ views/script/script.js
@@ -1,10 +1,13 @@
 const video = document.getElementById("video");
+const message = document.getElementById("message");
+const audio = document.getElementById("audio");
 
 const music = [
   { expression: "sad", name: "sad.mp3" },
   { expression: "happy", name: "happy.mp3" },
   { expression: "neutral", name: "neutral.mp3" },
 ];
+let prevEmot = null;
 
 window.addEventListener("DOMContentLoaded", async () => {
   navigator.mediaDevices.getUserMedia({ video: true }).then((stearm) => {
@@ -26,5 +29,24 @@ setInterval(async () => {
     result[0].expressions[a] > result[0].expressions[b] ? a : b
   );
 
+  if (emot != prevEmot) {
+    if (emot == "sad") {
+      message.innerHTML = "چرا نارحتی عزیزم؟ 😔";
+      audio.src = "../assets/music/sad.mp3";
+      prevEmot = emot;
+    }
+
+    if (emot == "happy") {
+      message.innerHTML = "خوشحالم که خوشحالی❤️";
+      audio.src = "../assets/music/happy.mp3";
+      prevEmot = emot;
+    }
+
+    if (emot == "neutral") {
+      message.innerHTML = "چقدر پوکر فیس😂";
+      audio.src = "../assets/music/neutral.mp3";
+      prevEmot = emot;
+    }
+  }
   console.log(emot);
 }, 1000);
